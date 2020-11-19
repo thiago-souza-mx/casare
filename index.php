2cb941b2337459fd989b1497e24c7bea4161babf
thiago-souza-mx <thiagoraouza@gmail.com>
2020-11-19 17:40:20 -0300

    initial

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+$ch = curl_init('https://www.casare.me/thaisandrafa');
+
+curl_setopt_array($ch, [
+
+    // Equivalente ao -X:
+    CURLOPT_CUSTOMREQUEST => 'GET',
+
+    // Equivalente ao -H:
+    CURLOPT_HTTPHEADER => [
+        'JsonOdds-API-Key: yourapikey'
+    ],
+
+    // Permite obter o resultado
+    CURLOPT_RETURNTRANSFER => 1,
+]);
+
+$resp = curl_exec($ch);
+
+$res = explode('<section id="videos">',$resp);
+$res2 = explode('</section>',$res[1],2);
+$no_video = $res[0].$res2[1];
+
+$res = explode('<section id="gift-lists">',$no_video); 
+$res2 = explode('</section>',$res[1],2);
+$no_lista = $res[0].$res2[1];
+
+$html = $no_lista;
+
+curl_close($ch);
+
+print_r( $html);
