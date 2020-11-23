8b0f9b154e3b4abecc5a82be3fb0ed0c0555c190
thiago-souza-mx <thiagoraouza@gmail.com>
2020-11-23 02:05:12 -0300

    add ajustes de botoes

--- lista/add/index.php
+++ lista/add/index.php
@@ -3,18 +3,14 @@
 header("Access-Control-Allow-Headers: *"); 
 header("Access-Control-Allow-Origin: *"); 
 
-$root = 'http://casamento.thaisethiago.tk/';
-if(isset($_SERVER["AMBIENTE"]) && $_SERVER["AMBIENTE"] == "PODUCAO")
-    $root = 'http://'.$_SERVER['HTTP_HOST'];
-
 if(isset($_POST['body']) && !empty($_POST['body'])){
 	$data = $_POST['body'];
 }else{
 	$data = file_get_contents("php://input");
 }
 
-file_put_contents($root."/lista/lista.json", $data );
+file_put_contents("lista.json", $data );
 
-print_r( file_get_contents( $root."/lista/lista.json" ) ); 
+print_r( file_get_contents( "lista.json" ) ); 
 
 //print_r($data);
